c8740c8ac1c1888b4306c44df3a78bad09f53e7a
tujade <jade.mundim@gmail.com>
2022-05-23 20:08:58 -0300

    Scroll Up  Set Up :bookmark:

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,6 +66,12 @@ linkProducts.forEach(l => l.addEventListener('click', activeProducts))
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 350) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
